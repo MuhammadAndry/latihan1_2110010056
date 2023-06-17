2cf6e3cb0aa67c02491ab9c1008fe46615e9afb2
MuhammadAndry <mhmmdndrybisnis@gmail.com>
2023-06-17 18:03:55 +0800

    membuat konstruktor dan setPenjumlahan

--- src/latihan1/matematika_beraksi.java
+++ src/latihan1/matematika_beraksi.java
@@ -10,5 +10,15 @@ package latihan1;
  * @author User
  */
 public class matematika_beraksi {
+    double bil1,bil2;
+
+    public matematika_beraksi(double bil1, double bil2) {
+        this.bil1 = bil1;
+        this.bil2 = bil2;
+    }
+    double setPenjumlahan (){
+        return bil1+bil2;
+        
+    }
     
 }
